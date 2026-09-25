6733671fc17fab7244a93c9ada996bcc7001238c
4-panel <ayoteamayo@gmail.com>
2026-09-25 14:53:51 +0330

    Update database.py

--- database.py
+++ database.py
@@ -21,11 +21,19 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    uuid = Column(String)
-    expires = Column(String)
+    username = Column(String, unique=True, nullable=False)
+    uuid = Column(String, nullable=False)
+    expires = Column(String, nullable=False)
     traffic_limit = Column(Integer, default=0)
     enabled = Column(Boolean, default=True)
+
+
+class Admin(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
 
 
 Base.metadata.create_all(bind=engine)
